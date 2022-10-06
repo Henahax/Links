0df979395f8bfa8504e6d3fa7d4496f4307c52d4
Henahax <mail@henahax.de>
2022-10-06 09:21:35 +0200

    Links 2022

--- script.ts
+++ script.ts
@@ -1,52 +1,66 @@
 var data = JSON.parse("");
-function generatePage() {
-    $.getJSON("data.json", function (json) {
-        data = json;
-        generateHead();
-        generateProfile();
-        generateLinks();
-        generateFooter();
-    })
-        .fail(function (jqXHR, textStatus, error) {
-        console.log("Post error: " + error);
-    });
+
+function generatePage() : void{
+ $.getJSON("data.json", function(json){
+    data = json;
+    generateHead();
+    generateProfile();
+    generateLinks();
+    generateFooter();
+ })
+ .fail(function (jqXHR, textStatus, error) {
+    console.log("Post error: " + error);
+});
 }
-function generateHead() {
+
+function generateHead(){
     $('head').append('todo');
 }
-function generateProfile() {
-    $.each(data.body.profile, function (key, val) {
+
+function generateProfile(){
+
+    $.each(data.body.profile, function(key, val){
+    
         var profileItem = $("<div></div>");
         profileItem.addClass(key.toString());
         profileItem.contents = val;
         $('#profile').append(profileItem);
     });
 }
+
 function generateLinks() {
-    $.each(data.body.links, function (key, val) {
+    $.each(data.body.links, function(key, val){
+
         var link = $("<a></a>");
         link.addClass("button");
         link.addClass(key.toString());
         link.attr("href", val.link);
+
         var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
         svg.setAttribute("width", "20");
         svg.setAttribute("height", "20");
-        switch (key) {
+
+        switch(key) {
             case "discord":
                 svg.setAttribute("viewBox", "0 0 640 512");
-                break;
+              break;
             default:
                 svg.setAttribute("viewBox", "0 0 512 512");
-        }
+            }
+
         var path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
         path.setAttribute("d", "");
+
         svg.append(path);
         link.append(svg);
+
         link.append(val.description);
+
         $("#links").append(link);
     });
 }
-function generateFooter() {
+
+function generateFooter(){
     $('footer').append('todo');
 }
